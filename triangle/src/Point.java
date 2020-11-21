4ae099417c17a1c4d99ebb55f021caae6598fe30
Stanislav <55299328+Imsamaboy@users.noreply.github.com>
2020-11-21 17:26:13 +0300

    Added logic in classes
    Added logic in Triangle class;
    Added logic in Point class;
    Added logic in ReadWriteUtil class;

--- triangle/src/Point.java
+++ triangle/src/Point.java
@@ -1,2 +1,17 @@
 public class Point {
+    double x;
+    double y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
 }
