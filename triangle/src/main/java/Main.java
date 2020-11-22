32903d8a145ec10907d6bca3545f0b88c841041e
Stanislav <55299328+Imsamaboy@users.noreply.github.com>
2020-11-22 20:41:20 +0300

    ADDED LOGIC IN CLASSES
    1) I have added parse-functions in main class.
    2) I have changed methods in Triangle class
    3) I have added getters and toStrings

---
+++ triangle/src/main/java/Main.java
@@ -0,0 +1,61 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static ArrayList<ArrayList<Point>> parseListOfStrings(List<String> listOfStrings) {
+        List<List<String>> filteredList = listOfStrings.stream()
+                                                    .map(x -> Arrays.asList(x.split(" ")))
+                                                    .filter(x -> x.size() == 6)
+                                                    .collect(Collectors.toList());
+
+        List<List<Double>> convertedFilteredList = filteredList.stream()
+                                                                .map(list -> list.stream()
+                                                                                .map(Double::parseDouble)
+                                                                                .collect(Collectors.toList()))
+                                                                .collect(Collectors.toList());
+
+        ArrayList<ArrayList<Point>> listOfPoints = new ArrayList<ArrayList<Point>>();
+        for (List<Double> list : convertedFilteredList) {
+
+            Point point_1 = new Point(list.get(0), list.get(1));
+            Point point_2 = new Point(list.get(2), list.get(3));
+            Point point_3 = new Point(list.get(4), list.get(5));
+
+            //ArrayList<Point> temp = new ArrayList<Point>(Arrays.asList(point_1, point_2, point_3));
+            listOfPoints.add(new ArrayList<Point>(Arrays.asList(point_1, point_2, point_3)));
+        }
+
+        return listOfPoints;
+    }
+
+    public static List<Triangle> getListOfTriangles(ArrayList<ArrayList<Point>> listOfPoints) {
+        List<Triangle> listOfTriangles = listOfPoints.stream()
+                                                     .map(list -> new Triangle(list.get(0),
+                                                                               list.get(1),
+                                                                               list.get(2)))
+                                                     .collect(Collectors.toList());
+        listOfTriangles.forEach(System.out::println);
+        return listOfTriangles;
+    }
+
+    public static List<Triangle> getIsoscelesTriangleWithMaxSquare(List<Triangle> listOfTriangles) {
+        return listOfTriangles.stream()
+                .sorted(Comparator.comparingDouble(Triangle::getSquare))
+                .limit(1)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String path = "E:\\__Java\\java_au\\triangle\\1.txt";
+        List<String> listOfStrings = ReadWriteUtil.read(path);
+        ArrayList<ArrayList<Point>> listOfPoints = parseListOfStrings(listOfStrings);
+        List<Triangle> listOfTriangles = getListOfTriangles(listOfPoints);
+
+        System.out.println(getIsoscelesTriangleWithMaxSquare(listOfTriangles).get(0));
+    }
+}
---
+++ triangle/src/main/java/Point.java
@@ -0,0 +1,25 @@
+public class Point {
+    private final double x;
+    private final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.x;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
